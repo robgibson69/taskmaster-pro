53fa7e297643cf5a4403b77168da74b74798335f
Gibby <rgibson@bulldogsolutions.ca>
2021-09-15 20:31:40 -0400

    added drag and drop feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,7 @@ var loadTasks = function() {
 		};
 	}
 
-	// loop over object properties
+	// loop over object properties 
 	$.each(tasks, function(list, arr) {
 			console.log(list, arr);
 			// then loop over sub-array
@@ -40,6 +40,7 @@ var loadTasks = function() {
 		});
 	});
 };
+
 
 var saveTasks = function() {
 	localStorage.setItem("tasks", JSON.stringify(tasks));
@@ -179,8 +180,72 @@ $(".list-group").on("blur", "input[type='text']", function() {
 	$(this).replaceWith(taskSpan);
 });
 
+// ****** MAKE COLUMNS SORTABLE (DRAG/DROP) ****** //
+$(".card .list-group").sortable({
+	connectWith: $(".card .list-group"),
+	scroll: false,
+	tolerance: "pointer",
+	herlper: "clone",
+	activate: function(event) {
+		console.log("activate", this);
+	},
+	deactivate: function(event) {
+		console.log("deactivate", this);
+	},
+	over: function(event) {
+		console.log("over", event.target);
+	},
+	out: function(event) {
+		console.log("out", event.target);
+	},
+	update: function(event) {
+		var tempArr = [];
+
+		// loop over current set of children in sortable list
+		$(this).children().each(function() {
+			var text = $(this)
+				.find("p")
+				.text()
+				.trim();
+
+			var date = $(this)
+				.find("span")
+				.text()
+				.trim();
+
+		// add task data to the temp array as an object
+			tempArr.push({
+				text: text,
+				date: date
+			});
+		});
+
+		// trim down list's ID to match object property
+		var arrName = $(this)
+		.attr("id")
+		.replace("list-", "");
+		
+		// update array on tasks object and save
+		tasks[arrName] = tempArr;
+		saveTasks();
+	}
+});
 
 
+// ******  DRAG AND DROP DELETE TASK  ****** //
+$("#trash").droppable({
+	accept: ".card .list-group-item",
+	tolerance: "pointer", 
+	drop: function(event, ui) {
+		ui.draggable.remove();
+	},
+	over: function(event, ui) {
+		console.log("over");
+	},
+	out: function(event, ui) {
+		console.log("out");
+	}
+});
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
